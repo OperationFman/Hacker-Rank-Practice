48f0693e14d5aee3e82e6882c55fd6a948e767ad
Franklin Moon <franklin.m.moon@gmail.com>
2021-02-28 09:28:55 +1100

    Update Day0-RepeatedString.py

--- Day0-RepeatedString.py
+++ Day0-RepeatedString.py
@@ -1,4 +1,32 @@
-str = 'blessed'
+import math
+import os
+import random
+import re
+import sys
 
-for i in str:
-    print(i)
+# Complete the repeatedString function below.
+def repeatedString(s, n):
+    a=0
+    m = s.count('a')
+    if m==0:
+        return 0
+    elif m==1 and len(s)==1:
+        return n
+    else:
+        r=int(n/len(s))
+        rr=int(n%len(s))
+        return(m*r+s[:rr].count('a'))
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    n = int(input())
+
+    result = repeatedString(s, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
