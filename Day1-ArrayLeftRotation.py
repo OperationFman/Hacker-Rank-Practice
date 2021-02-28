2f99d579e99f371c7e78d40b1e0e7f23d5557506
Franklin Moon <franklin.m.moon@gmail.com>
2021-03-01 09:46:14 +1100

    Create Day1-ArrayLeftRotation.py

---
+++ Day1-ArrayLeftRotation.py
@@ -0,0 +1,36 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the rotLeft function below.
+def pop(a):
+    storage = a[0]
+    a.pop(0)
+    a.append(storage)
+    return a
+
+def rotLeft(a, d):
+    array = a
+    for _ in range(0, d):
+        array = pop(array)
+    return array
+        
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nd = input().split()
+
+    n = int(nd[0])
+
+    d = int(nd[1])
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = rotLeft(a, d)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
